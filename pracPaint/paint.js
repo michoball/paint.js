122165c991d414600673bebd4bb8e8019b0f50c3
michoball <myunghun0114@gmail.com>
2021-05-06 13:48:24 +0900

    ADD stroke range function & filling mode Btn style

--- pracPaint/paint.js
+++ pracPaint/paint.js
@@ -1,22 +1,30 @@
 const canvas = document.getElementById("JsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "black";
 const CANVAS_SIZE_X = 1500;
 const CANVAS_SIZE_Y = 1000;
 
 let painting = false;
+let filling = false;
 
 canvas.width = CANVAS_SIZE_X;
 canvas.height = CANVAS_SIZE_Y;
 
+ctx.fillStyle = "wheat";
+ctx.fillRect(0, 0, CANVAS_SIZE_X, CANVAS_SIZE_Y);
 ctx.fillStyle = INITIAL_COLOR;
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 5;
 
 function startPainting(event) {
-  painting = true;
+  if (event.button === 0) {
+    painting = true;
+  }
 }
 function stopPainting(event) {
   painting = false;
@@ -41,6 +49,21 @@ function handleColorClick(event) {
   ctx.fillStyle = colorr;
 }
 
+function handleRange(event) {
+  const stroke = event.target.value;
+  ctx.lineWidth = stroke;
+}
+
+function handleMode(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "FILL";
+  } else {
+    filling = true;
+    mode.innerText = "PAINT";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -51,3 +74,11 @@ if (canvas) {
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleMode);
+}
